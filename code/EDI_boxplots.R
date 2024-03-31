b59b6c5c0185fe4b12da0950b4a3a16292a5fe99
Anna <anna.schmidt@uvm.edu>
2024-03-31 15:43:03 -0400

    creating boxplots of parameters by month, and trying out violin plots

--- code/EDI_boxplots.R
+++ code/EDI_boxplots.R
@@ -6,19 +6,15 @@ library(tidyverse)
 
 # uses master_times data frame created in create_mastersheet.R and time_manipulations.R
 
-# Boxplot of water.temperature each month
-master_times %>%
-  ggplot(aes(x = water.temperature, group = month_profiledatetime)) +
-  geom_boxplot()
-
 # Create subset of just our parameter columns
 subset <- master_times %>%
   select(c(water.temperature:photosynthetically.active.radiation.up))
 
-# Boxplots of our parameter values
+# Boxplots of all of our parameter values on one plot
+plot.new()
 par(mfrow = c(3, 3))  # setting up the plotting layout
 lapply(names(subset), function(col) {
-  boxplot(data[[col]], main = col, ylab = "Value")
+  boxplot(subset[[col]], main = col, ylab = "Value")
 })
 
 # Most obvious outliers:
@@ -26,4 +22,59 @@ lapply(names(subset), function(col) {
 # phycocyanin > 4 (six outliers)
 # conductivity < 200 (one outlier)
 
-# Anna will work on creating boxplots by month, and work on violin plots
+#####################################
+# Boxplots by month for each variable
+#####################################
+
+# Make month a character instead of numeric variable
+master_times$month_profiledatetime <- as.character(master_times$month_profiledatetime)
+
+# Boxplot of water.temperature each month
+master_times %>%
+  ggplot(aes(x = month_profiledatetime, y = water.temperature)) +
+  geom_boxplot()
+
+# Boxplot of conductivity each month
+master_times %>%
+  ggplot(aes(x = month_profiledatetime, y = conductivity)) +
+  geom_boxplot()
+
+# Boxplot of pH.value each month
+master_times %>%
+  ggplot(aes(x = month_profiledatetime, y = pH.value)) +
+  geom_boxplot()
+
+# Boxplot of chlorophyll.a each month
+master_times %>%
+  ggplot(aes(x = month_profiledatetime, y = chlorophyll.a)) +
+  geom_boxplot()
+
+# Boxplot of phycocyanin each month
+master_times %>%
+  ggplot(aes(x = month_profiledatetime, y = phycocyanin)) +
+  geom_boxplot()
+
+# Boxplot of oxygen.concentration each month
+master_times %>%
+  ggplot(aes(x = month_profiledatetime, y = oxygen.concentration)) +
+  geom_boxplot()
+
+# Boxplot of photosynthetically.active.radiation.up each month
+master_times %>%
+  ggplot(aes(x = month_profiledatetime, y = photosynthetically.active.radiation.up)) +
+  geom_boxplot()
+
+########################################
+# Violin plots by month for each variable
+########################################
+
+# Violin plot of water.temperature each month
+master_times %>%
+  ggplot(aes(y = water.temperature, x = month_profiledatetime, fill = month_profiledatetime)) +
+  geom_violin()
+
+# etc...for the other variables
+
+# See http://www.sthda.com/english/wiki/ggplot2-violin-plot-quick-start-guide-r-software-and-data-visualization for more customization of violin plots
+# Do we like violin plots or boxplots better?
+# Could also split up by mesocosm, and/or specified.depth, instead of month, to do more EDI
